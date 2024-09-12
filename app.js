fd33731775fc8a423af4725fc1d7a07550018450
gayangg <gayang.fi@gmail.com>
2024-09-12 17:34:31 +0300

    8-deploment

--- app.js
+++ app.js
@@ -1,21 +1,7 @@
-// import { serve } from "./deps.js";
-
-// const handleRequest = (request) => {
-//   return new Response("My app deplyment on Render....!");
-// };
-
-// serve(handleRequest, { port: 7777 });
-
-import { serve } from "https://deno.land/std@0.222.1/http/server.ts";
-
-let count = 0;
+import { serve } from "./deps.js";
 
 const handleRequest = (request) => {
-  if (request.method === "POST") {
-    count++;
-  }
-
-  return new Response(count);
+  return new Response("Seeking truths beyond meaning of life, you will find 43.");
 };
 
 serve(handleRequest, { port: 7777 });
